337840101b589f1830c23e737190adfbe542ac76
Daniel <daniel.jalonso07@gmail.com>
2025-10-21 19:46:24 +0200

    Clase 21/10/25

--- src/ProgramacionTema2/Ejercicios_2/Ejercicio_9.java
+++ src/ProgramacionTema2/Ejercicios_2/Ejercicio_9.java
@@ -1,11 +1,27 @@
 package ProgramacionTema2.Ejercicios_2;
 
+import java.util.Scanner;
+
 //Mostrar los N primeros términos de la serie de Fibonacci. N se definirá como una variable.
 
 public class Ejercicio_9 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Cuantos numeros de la serie de Fibonacci quieres que se muestren por pantalla?");
+        int cantidad = sc.nextInt();
+
+        int num1 = 0;
+        int num2 = 1;
+
+        System.out.print(num1 + ", ");
+        for (int i = 1; i <= cantidad; i++) {
+            num1 = num2;
+            System.out.print(num2 + ", ");
+            num2 += num1;
+
+        }
 
     }
 }
--- src/ProgramacionTema2/Pruebas/Piramide.java
+++ src/ProgramacionTema2/Pruebas/Piramide.java
@@ -1,8 +1,8 @@
-package ProgramacionTema2.Ejercicios_2;
+package ProgramacionTema2.Pruebas;
 
 import java.util.Scanner;
 
-public class Ejercicio_5 {
+public class Piramide {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -10,12 +10,20 @@ public class Ejercicio_5 {
         System.out.println("Escribe un numero");
         int numVeces = sc.nextInt();
 
+
         for (int i = 1; i <= numVeces; i++) {
             for (int a = 1; a <= i; a++) {
-                System.out.print(a + " ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = numVeces - 1; i >= 1; i--) {
+            for (int a = 1; a <= i; a++) {
+                System.out.print("*");
             }
             System.out.println();
         }
 
+        sc.close();
     }
 }
