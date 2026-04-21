bf65a5a1d28858fe4db8ecf3da6d0072c6b41c18
danieljalonso <daniel.jalonso@gmail.com>
2026-04-21 20:04:27 +0200

    clase 21/4/2026

---
+++ src/ProgramacionTema7/ejercicioPrueba/GestorInstituto.java
@@ -0,0 +1,48 @@
+package ProgramacionTema7.ejercicioPrueba;
+
+import java.sql.*;
+
+public class GestorInstituto {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/instituto_db";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = ""; // Que cada alumno ponga la suya
+    private static Connection conexion;
+
+    // TODO Paso 1: Crear el método conectar() y desconectar()
+    // Mantenemos una única conexión en toda la aplicación
+
+    // TODO Paso 2: Crear el método registrarAlumno(String nombre, String email)
+    // Alta de un alumno en su tabla correspondiente
+
+    // TODO Paso 3: Crear el método matricularAlumno(int idAlumno, int idAsignatura)
+    // Aquí estamos insertando una relación N:N
+
+    // TODO Paso 4: Crear el método mostrarAsignaturasDeAlumno(int idAlumno)
+    // Select con join...
+
+    // TODO Paso 5: Crear el método darDeBajaAlumno(int idAlumno)
+    // Delete...
+
+    public static void main(String[] args) {
+        GestorInstituto gestor = new GestorInstituto();
+        // Aquí iremos probando los métodos paso a paso
+
+        Connection conexion = conectar();
+        System.out.println("Estamos conectados a la base de datos");
+    }
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.musql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.err.println("No se ha encontrado el driver de MySQL");
+        } catch (SQLException e) {
+            System.out.println("Error de SQL al conectar, " + e.getMessage());
+        }
+        return conexion;
+    }
+
+
+}
